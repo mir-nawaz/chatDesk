d22b9a9e231771a928b86919dce11d5aeffcba80
Mir Nawaz <inbox.mirnawaz@gmail.com>
2018-05-15 12:11:13 +0400

    WIP

--- bot/dialog/intent/proactive.js
+++ bot/dialog/intent/proactive.js
@@ -1,37 +1,22 @@
 'use strict';
 
-const users = require('./data/Users.json');
-
-function getLastProblemForUser(userId, session) {
-  return 'last problem';
-}
-
-function getRandomUserJSON(users) {
-  const max = users.length;
-  return users[getRandomInt(0, max)];
-}
-
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; // The maximum is exclusive and the minimum is inclusive
-}
+const _ = require('lodash');
 
 module.exports = [
   function(session, args, next) {
 
-    const userJSON = getRandomUserJSON(users);
-    const responseText = `Mr ${userJSON.user} from ${userJSON.org}, Welcome to Etisalat help desk, How can i help you!!`;
-    session.userData.user = userJSON.user;
-    session.userData.org = userJSON.org;
+    const { title = 'Mr.', company, name } = _.get(session, 'message.address.user', {});
+
+    let responseText = `${title} ${name}`;
+
+    responseText += company ? ` from ${company}, ` : ', ';
+    responseText += 'Welcome to Etisalat help desk, How can i help you!!';
 
     session.send(responseText);
 
-    const responseText2 = getLastProblemForUser(userJSON.user, session);
-    if (responseText2 !== '') {
-      session.send(responseText2 + ' How may i help you now?');
-      session.endDialog();
-    }
+    // TODO save last problem of user and send show to user
+    // session.send('last problem' + ' How may i help you now?');
+    session.endDialog();
 
   }
 ];
