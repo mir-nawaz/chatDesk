1f892a4119f777fb6278bd3157940201cb02d94a
Mir Nawaz <inbox.mirnawaz@gmail.com>
2018-05-19 16:06:43 +0400

    empty conversationData on end

--- bot/dialog/intent/goodBye.js
+++ bot/dialog/intent/goodBye.js
@@ -3,6 +3,7 @@
 module.exports = [
 
   function(session, results, next) {
+    session.conversationData = {};
     session.send('Your Issues has been reported. Thank you for using Etisalat !!!');
   }
 
