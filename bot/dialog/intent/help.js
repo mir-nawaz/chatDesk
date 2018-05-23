f423b6fe949bff6048f43f615a457cc37334290f
Mir Nawaz <inbox.mirnawaz@gmail.com>
2018-05-23 15:22:52 +0400

    help menu

--- bot/dialog/intent/help.js
+++ bot/dialog/intent/help.js
@@ -8,20 +8,23 @@ const stringInject = require('../helper/stringInject');
 module.exports = help;
 
 function help(session) {
-  {
-    const helpMsg = message.help;
-    const name = session.message.address.user.name;
-    const suggestions = [];
-    for (const msg of helpMsg) {
-      suggestions.push(builder.CardAction.imBack(session, msg, msg));
-    }
-    const msg = new builder.Message(session)
-      .text(stringInject(lang.getText('helpMsg'), { name: name }))
-      .suggestedActions(
-        builder.SuggestedActions.create(
-          session, suggestions
-        ));
-    session.send(msg);
-    session.endDialog();
+
+  const helpMsg = message.help;
+  const name = session.message.address.user.name;
+  session.send(stringInject(lang.getText('helpMsg'), { name: name }));
+  const suggestions = [];
+  const msg = new builder.Message(session);
+  for (const msg of helpMsg) {
+    suggestions.push(new builder.HeroCard(session)
+      .text(msg)
+      .buttons([
+        builder.CardAction.imBack(session, msg, 'Get Started')
+      ]));
   }
+  msg.attachmentLayout(builder.AttachmentLayout.carousel);
+  msg.attachments(suggestions);
+
+  session.send(msg);
+  session.endDialog();
+
 }
